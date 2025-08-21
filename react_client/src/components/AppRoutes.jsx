203efd46a4e5a04787ededb08f4bb7c38120d514
Anderson A Lopes <romalopes@yahoo.com.br>
2025-08-21 14:12:36 +1000

    new post form

--- react_client/src/components/AppRoutes.jsx
+++ react_client/src/components/AppRoutes.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import PostList from "../features/posts/PostList";
 import PostDetails from "../features/posts/PostDetails";
+import NewPostForm from "../features/posts/NewPostForm";
 
 function AppRoutes() {
   return (
@@ -9,7 +10,7 @@ function AppRoutes() {
       <Routes>
         <Route path="/" element={<PostList />} />
         <Route path="/posts/:id" element={<PostDetails />} />
-        <Route path="/new" element={<h1>New Post Form</h1>} />
+        <Route path="/new" element={< NewPostForm />} />
       </Routes>
     </>
   );
