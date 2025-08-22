ef157e6e4c19febc2e6d748f3b88d5aaa83ba8ab
Anderson A Lopes <romalopes@yahoo.com.br>
2025-08-22 12:55:26 +1000

    creating api_service

--- react_client/src/features/posts/PostDetails.jsx
+++ react_client/src/features/posts/PostDetails.jsx
@@ -2,7 +2,10 @@ import React, { useState, useEffect } from "react";
 import { useParams, useNavigate, Link } from "react-router-dom";
 
 // import "./PostList.css"; // Assuming you have some styles for the PostList
-import { API_URL } from "../../constants";
+import {
+  fetchDeletePost as fetchDeletePost_by_id,
+  fetchPost as fetchPost_by_id,
+} from "../../api_services/postService";
 
 function PostDetails() {
   const { id } = useParams(); // ✅ pega o id da URL
@@ -15,13 +18,15 @@ function PostDetails() {
     const fechCurrentPost = async () => {
       try {
         // const response = await fetch("${API_URL}/${id}");
-        const response = await fetch(`${API_URL}/${id}`); // ✅ usa id aqui
-        if (response.ok) {
-          const json = await response.json();
-          setPost(json);
-        } else {
-          throw response;
-        }
+        // const response = await fetch(`${API_URL}/${id}`); // ✅ usa id aqui
+        // if (response.ok) {
+        //   const json = await response.json();
+        //   setPost(json);
+        // } else {
+        //   throw response;
+        // }
+        const json = await fetchPost_by_id(id);
+        setPost(json);
       } catch (e) {
         setError("Error occurred..." + e);
         console.log("Error occurred..." + e);
@@ -38,11 +43,10 @@ function PostDetails() {
   const deletePost = async (id) => {
     try {
       // Delete the post from the server using a DELETE request http://localhost:3000/api/v1posts/:id
-      await fetch(`${API_URL}/${id}`, {
-        method: "DELETE",
-      });
-      console.log("Deleted with success");
-      // setPosts((prevPosts) => prevPosts.filter((post) => post.id !== id));
+      // await fetch(`${API_URL}/${id}`, {
+      //   method: "DELETE",
+      // });
+      await fetchDeletePost_by_id(id);
       navigate(`/`);
     } catch (e) {
       setError("Error occurred..." + e);
--- react_client/src/features/posts/PostList.jsx
+++ react_client/src/features/posts/PostList.jsx
@@ -1,23 +1,37 @@
 import React, { useState, useEffect } from "react";
-import "./PostList.css"; // Assuming you have some styles for the PostList
 import { API_URL } from "../../constants";
 import { Link } from "react-router-dom";
+import { fetchAllPosts, fetchDeletePost } from "../../api_services/postService";
 
 function PostList() {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [, setError] = useState(null);
 
+  // useEffect(() => {
+  //   async function loadPosts() {
+  //     try {
+  //       const response = await fetch(API_URL);
+  //       if (response.ok) {
+  //         const json = await response.json();
+  //         setPosts(json);
+  //       } else {
+  //         throw response;
+  //       }
+  //     } catch (e) {
+  //       setError("Error occurred..." + e);
+  //       console.log("Error occurred..." + e);
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   }
+  //   loadPosts();
+  // }, []);
   useEffect(() => {
     async function loadPosts() {
       try {
-        const response = await fetch(API_URL);
-        if (response.ok) {
-          const json = await response.json();
-          setPosts(json);
-        } else {
-          throw response;
-        }
+        const data = await fetchAllPosts();
+        setPosts(data);
       } catch (e) {
         setError("Error occurred..." + e);
         console.log("Error occurred..." + e);
@@ -33,11 +47,12 @@ function PostList() {
 
   const deletePost = async (id) => {
     try {
+      await fetchDeletePost(id);
       // Delete the post from the server using a DELETE request http://localhost:3000/api/v1posts/:id
-      await fetch(`${API_URL}/${id}`, {
-        method: "DELETE",
-      });
-      console.log("Deleted with success");
+      // await fetch(`${API_URL}/${id}`, {
+      //   method: "DELETE",
+      // });
+      // console.log("Deleted with success");
       setPosts((prevPosts) => prevPosts.filter((post) => post.id !== id));
       // setPosts(posts.filter((post) => post.id !== id));
     } catch (e) {
