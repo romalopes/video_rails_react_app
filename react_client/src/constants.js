575f196be75da80a96ceae69cf012f5f34050dc5
Anderson A Lopes <romalopes@yahoo.com.br>
2025-08-28 17:52:24 +1000

    constants

--- react_client/src/constants.js
+++ react_client/src/constants.js
@@ -1,10 +1,10 @@
-// // export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000/api/v1/posts';
-// export const API_URL =
-//   process.env.NODE_ENV === "test"
-//     ? "http://mock-api.com"
-//     : import.meta.env.VITE_API_URL; // || 'http://localhost:3000/api/v1/posts';
+// export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000/api/v1/posts';
+export const API_URL =
+  process.env.NODE_ENV === "test"
+    ? "http://mock-api.com"
+    : import.meta.env.VITE_API_URL; // || 'http://localhost:3000/api/v1/posts';
 
-// export const SEARCH_API_URL =
-//   process.env.NODE_ENV === "test"
-//     ? "http://mocked-api-url"
-//     : import.meta.env.VITE_SEARCH_API_URL;
+export const SEARCH_API_URL =
+  process.env.NODE_ENV === "test"
+    ? "http://mocked-api-url"
+    : import.meta.env.VITE_SEARCH_API_URL;
